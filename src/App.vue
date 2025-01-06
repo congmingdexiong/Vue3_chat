ffa4ae4e6e32413ddfa8334a78e8568661d6e48f
aleng1106 <ltlobygg@163.com>
2025-01-06 21:36:07 +0800

    add chat layout

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,62 @@
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
-<template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        App
-        <RouterLink to="/">Chat</RouterLink>
-        <el-button type="primary">Primary</el-button>
-      </nav>
-    </div>
-  </header>
+<!-- <template>
 
-  <RouterView />
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header><RouterLink to="/">Chat</RouterLink></el-header>
+        <el-main>
+          <RouterView />
+           <el-button type="primary">Primary</el-button>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template> -->
+
+<template>
+  <el-container class="common-layout">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header><RouterLink to="/">Chat</RouterLink></el-header>
+      <el-main><RouterView /> </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <style>
-.color-red {
-  color: red;
+.common-layout {
+  height: 100%;
 }
+.el-container {
+  background-color: blue;
+}
+.el-header {
+  height: 3rem;
+  background-color: red;
+}
+.el-main {
+  background-color: green;
+}
+.el-aside {
+  background-color: blue;
+}
+.el-button {
+  display: block;
+}
+@media (max-width: 576px) {
+  .el-aside {
+    display: none;
+  }
+}
+/*
+
+@media (min-width: 1024px) {
+  .el-aside {
+    display: block;
+  }
+} */
 </style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="color-red">chat works!</div>
+  <div class="chat-wrapper">
+    <div class="chat-content">chat content</div>
+    <div class="chat-input-area">
+      <el-button type="primary">Primary</el-button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts" name="ChatView"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.chat-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: yellow;
+  .chat-content {
+    background-color: red;
+    height: 80%;
+  }
+
+  .chat-input-area {
+    background-color: orange;
+    height: 20%;
+  }
+}
+</style>
